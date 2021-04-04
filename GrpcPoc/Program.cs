727caed8637425fac29a09422c25f56dd72882e7
sandorbari-ppb <sandor.bari@paddypowerbetfair.com>
2021-04-04 19:55:06 +0100

    initial commit

---
+++ GrpcPoc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrpcPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
